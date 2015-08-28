57e9f25d317e9d39f7b9477d08441cd2540e1005
GiovanniPetrella <tangolikeamango1@gmail.com>
2015-08-27 19:25:07 -0700

    Many Methods and Instances
    Many Methods and Instances

---
+++ src/apples.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class apples {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		tuna tunaObject = new tuna();
+		System.out.println("Enter the name of your fist gf here: ");
+		String temp = input.nextLine();
+		tunaObject.setName(temp);
+		tunaObject.saying();
+	}
+}
